4b03cdd338decc0de87f5f844f87ee11891137a1
soroush-sakhaei <42448782+soroush-sakhaei@users.noreply.github.com>
2018-08-29 13:33:06 +0430

    add food.file

---
+++ src/p1/Food.java
@@ -0,0 +1,22 @@
+package p1;
+
+
+
+public class Food {
+	
+private Integer id;
+private String foodName;
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getFoodName() {
+	return foodName;
+}
+public void setFoodName(String foodName) {
+	this.foodName = foodName;
+}
+
+}
